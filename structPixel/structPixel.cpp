447fee5ad67bd22dcc0c60919c3d437e8cf8769d
Lance <53155359+Lancelof2019@users.noreply.github.com>
2022-11-10 08:29:37 +0100

    Add files via upload

---
+++ structPixel/structPixel.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+struct Pixel {
+    int val;
+    int x;
+    int y;
+
+    Pixel(int val, int x, int y) : val(val), x(x), y(y) {}
+    }
+};
+
+
+struct Compare{
+
+   bool operator()(Pixel &para1, Pixel &para2) {
+        return para1.val < para2.val;
+    }
+
+};
